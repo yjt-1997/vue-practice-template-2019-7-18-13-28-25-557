cfea2d88ba4d324bbe3616794daf979a54ea6d6a
YOUNG YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGYO7@OOCL.COM>
2019-07-19 16:05:20 +0800

    implement multi counter

--- src/Counter.vue
+++ src/Counter.vue
@@ -1,16 +1,26 @@
 <template>
-  <div id="app">
-    <Counter></Counter>
-    <Counter></Counter>
+  <div>
+    <button v-on:click="decrease">-</button>
+    <span>{{ count }}</span>
+    <button v-on:click="increase">+</button>
   </div>
 </template>
 
 <script>
-import Counter from './Counter.vue'
 export default {
   name: 'app',
-  components: {
-    Counter
+  data() {
+    return {
+      count :0,
+    }
+  },
+  methods : {
+    decrease(){
+      this.count--;
+    },
+    increase(){
+      this.count++;
+    }
   }
 }
 </script>

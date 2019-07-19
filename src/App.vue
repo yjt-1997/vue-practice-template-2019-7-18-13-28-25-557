b1fc824424806a8e2c691b5ee5906eed2da826af
YOUNG YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGYO7@OOCL.COM>
2019-07-19 15:53:34 +0800

    implement count

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="decrease">-</button>
+    <span>{{ count }}</span>
+    <button v-on:click="increase">+</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      count :0,
+    }
+  },
+  methods : {
+    decrease(){
+      this.count--;
+    },
+    increase(){
+      this.count++;
+    }
   }
 }
 </script>
